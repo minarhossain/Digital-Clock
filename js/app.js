6a0c356a4b11aac9640b7d5cc0ce0ba805725900
minarhossain <codewithmnr@gmail.com>
2023-04-06 16:35:06 +0600

    this is the last commit of digital clock

--- js/app.js
+++ js/app.js
@@ -2,13 +2,9 @@ console.log("Connected");
 
 function displayTime() {
     const date = new Date();
-
     const time = date.toLocaleTimeString();
-
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-
     const dateStr = date.toLocaleDateString(undefined, options);
-
     document.getElementById("clock").innerHTML = time + "<br>" + dateStr;
 }
 setInterval(displayTime, 1000);
