3e91d0e4e4a6810d15426dca885366ba8f4b90da
minarhossain <codewithmnr@gmail.com>
2023-04-06 16:20:26 +0600

    digital clock update

---
+++ js/app.js
@@ -0,0 +1,15 @@
+console.log("Connected");
+
+function displayTime() {
+    const date = new Date();
+
+    const time = date.toLocaleTimeString();
+
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+
+    const dateStr = date.toLocaleDateString(undefined, options);
+
+    document.getElementById("clock").innerHTML = time + "<br>" + dateStr;
+}
+setInterval(displayTime, 1000);
+// displayTime()
